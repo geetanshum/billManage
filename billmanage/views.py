f556da09437e3647d48b7a460d056eb0fc237024
deepakschauhan <deepaksingh0034602@gmail.com>
2021-06-19 09:57:45 -0700

    backup folder

--- billmanage/views.py
+++ billmanage/views.py
@@ -19,9 +19,8 @@ from django.contrib import messages
 def subscription(request):
     template = loader.get_template('webforntjoinform/index.html')
     sForm = billformDetail()
-    context = {"joinform": sForm}
+    context = {"joinform":sForm}
     return HttpResponse(template.render(context, request))
-
 
 def addjoinsubscription(request):
     s = bill()
@@ -35,7 +34,6 @@ def addjoinsubscription(request):
     template = loader.get_template('webforntjoinform/alert.html')
     context = {}
     return HttpResponse(template.render(context, request))
-
 
 def reportbill(request):
     resp = Response()
@@ -52,30 +50,24 @@ def reportbill(request):
         print(status)
         if status == "" and remark == "":
             # print(fromdate)
-            itbill = bill.objects.filter(
-                date__gte=fromdate, date__lte=todate).order_by("date")
+            itbill = bill.objects.filter(date__gte=fromdate,date__lte=todate).order_by("date")
 
         elif str(request.POST.get("remark")) == "" and status != "":
-            itbill = bill.objects.filter(
-                date__gte=fromdate, date__lte=todate, status=status).order_by("date")
+            itbill = bill.objects.filter(date__gte=fromdate,date__lte=todate, status=status).order_by("date")
 
         elif remark != "" and status == "":
-            itbill = bill.objects.filter(
-                date__gte=fromdate, date__lte=todate, remark__startswith=remark).order_by("date")
+            itbill = bill.objects.filter(date__gte=fromdate,date__lte=todate,remark__startswith = remark).order_by("date")
             print("hello")
-
+            
         else:
-            itbill = bill.objects.filter(
-                date__gte=fromdate, date__lte=todate, remark__startswith=remark, status=status).order_by("date")
+            itbill = bill.objects.filter(date__gte=fromdate,date__lte=todate,remark__startswith = remark, status=status).order_by("date")
 
-        r.fromQueryset(itbill, {"code": "id", "bill": "billno", "date": {
-                       "name": "date", "format": "%d-%m-%Y"}, "remark": "remark", "amount": "amount", "status": "status"})
+        r.fromQueryset(itbill,{"code":"id","bill":"billno","date":{"name":"date","format":"%d-%m-%Y"},"remark":"remark","amount":"amount", "status":"status"})
         resp.success("data ok")
         resp.setExtra(r.data)
         print(itbill)
         return HttpResponse(resp.getJson())
-    return render(request, "webforntjoinform/report.html", context=contaxt)
-
+    return render(request,"webforntjoinform/report.html",context=contaxt)
 
 def print_view(request):
     resp = Response()
@@ -89,29 +81,17 @@ def print_view(request):
         fromdate = datetime.strptime(fromdt, '%Y-%m-%d')
         todate = datetime.strptime(todt, '%Y-%m-%d')
         # print(remark)
-        if str(request.POST.get("remark")) == "" and status == "":
+        if str(request.POST.get("remark")) == "":
             # print(fromdate)
-            itbill = bill.objects.filter(
-                date__gte=fromdate, date__lte=todate).order_by("date")
-
-        elif status == "Paid":
-            itbill = bill.objects.filter(
-                date__gte=fromdate, date__lte=todate, status="Paid").order_by("date")
-        elif status == "Unpaid":
-            itbill = bill.objects.filter(
-                date__gte=fromdate, date__lte=todate, status="Unpaid").order_by("date")
-
-        else:
-            itbill = bill.objects.filter(
-                date__gte=fromdate, date__lte=todate, remark__startswith=remark, status=status).order_by("date")
-        r.fromQueryset(itbill, {"date": {"name": "date", "format": "%d-%m-%Y"},
-                       "remark": "remark", "amount": "amount", "status": "status"})
+            itbill = bill.objects.filter(date__gte=fromdate,date__lte=todate).order_by("date")
+        else:  
+            itbill = bill.objects.filter(date__gte=fromdate,date__lte=todate,remark__startswith = remark).order_by("date")
+        r.fromQueryset(itbill,{"date":{"name":"date","format":"%d-%m-%Y"},"remark":"remark","amount":"amount", "status":"status"})
         resp.success("data ok")
         resp.setExtra(r.data)
         print(itbill)
-        return HttpResponse(resp.getJson())
-    return render(request, "webforntjoinform/print.html", context=contaxt)
-
+        return HttpResponse(resp.getJson())      
+    return render(request, "webforntjoinform/print.html",context=contaxt)
 
 def statusUpdate(request):
     dt = request.POST.get("data")
@@ -120,18 +100,18 @@ def statusUpdate(request):
     # resp = Response()
     # r = Recordset()
     data = bill.objects.filter(id=id)
-    obj = get_object_or_404(bill, id=id)
+    obj = get_object_or_404(bill ,id=id)
     if obj.status == 'U':
         data = bill.objects.filter(id=id).update(status='P')
         print("Marked as Paid")
-        messages.info(request, "Marked as Paid")
+        messages.info(request,"Marked as Paid")
         return HttpResponse("Marked as Paid")
     # elif obj.status == 'P':
     #     data = bill.objects.filter(id=id).update(status='U')
     #     print("Marked as UnPaid")
     #     return HttpResponse("Marked as UnPaid")
     else:
-        messages.info(request, "Bill is already paid")
+        messages.info(request,"Bill is already paid")
         return HttpResponse("Bill is already paid")
     print(data)
     # r.fromQueryset(data,{"amount":"amount"})
